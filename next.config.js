9cb785d974e2d1c6cc26130c8efd47d81adf084e
MRizainF <icon.firdaus@gmail.com>
2025-05-13 15:14:58 +0700

    fix: fix css deployment

--- next.config.js
+++ next.config.js
@@ -1,19 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    optimizeCss: false
-  },
   output: 'standalone',
-  distDir: '.next',
-  trailingSlash: false,
-  reactStrictMode: true,
-  webpack: (config) => {
-    config.module.rules.push({
-      test: /\.css$/,
-      use: ['style-loader', 'css-loader', 'postcss-loader'],
-    });
-    return config;
-  }
+  reactStrictMode: true
 }
 
 module.exports = nextConfig 
